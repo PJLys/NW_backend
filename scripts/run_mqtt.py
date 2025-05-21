6dd470be09b347647bdd8201b7fc9bbd65a96a7a
Pieter-Jan Lyssens <pjlys@duck.com>
2025-05-21 15:34:13 +0200

    Renaming and restructuring of python packages

--- scripts/run_mqtt.py
+++ scripts/run_mqtt.py
@@ -1,4 +1,4 @@
-from py_mqtt_client.mqtt import create_mqtt_client, connect_and_loop
+from mqtt_client.mqtt import create_mqtt_client, connect_and_loop
 
 if __name__ == "__main__":
     pyClient = create_mqtt_client()
